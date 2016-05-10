6c05ea809db603c941aacb0a2fdafe9284fcfb8c
Spuns927 <spunikgaming@gmail.com>
2016-05-10 10:07:01 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -243,7 +243,13 @@ exports.commands = {
 
 		if (!message) message = '|pm|' + user.getIdentity() + '|' + targetUser.getIdentity() + '|' + target;
 		user.send(message);
-		if (targetUser !== user) targetUser.send(message);
+		if (targetUser !== user) {
+			if (Users.ShadowBan.checkBanned(user)) {
+				Users.ShadowBan.addMessage(user, "Private to " + targetUser.getIdentity(), target);
+			} else {
+				targetUser.send(message);
+			}
+		}
 		targetUser.lastPM = user.userid;
 		user.lastPM = targetUser.userid;
 	},
