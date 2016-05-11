bedf59d77f4c1717e5adbfb8cc7f8542982072fb
Spuns927 <spunikgaming@gmail.com>
2016-05-11 15:35:26 -0400

    Update dnsbl.js

--- dnsbl.js
+++ dnsbl.js
@@ -14,7 +14,7 @@
 
 'use strict';
 
-const BLOCKLISTS = ['sbl.spamhaus.org', 'rbl.efnetrbl.org', '100tb.com', '.unknown-nohost', 'optonline.net'];
+const BLOCKLISTS = ['sbl.spamhaus.org', 'rbl.efnetrbl.org', '100tb.com', '.unknown-nohost', 'optonline.net', 'radiant.net'];
 
 let dns = require('dns');
 
